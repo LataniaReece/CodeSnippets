837f93a5990f493acd5f5836296d3db8b4584637
YOUR_USERNAME <latania.reece@outlook.com>
2019-12-16 07:57:29 -0500

    updated changes

---
+++ JAvascript/Json/json_basics/config.js
@@ -0,0 +1,11 @@
+export default
+{
+    "people":[
+      {"firstName":"John", 
+        "lastName":"Doe"},
+      {"firstName":"Anna", 
+        "lastName":"Smith"},
+      {"firstName":"Peter",
+         "lastName":"Jones"}
+    ]
+}
--- JAvascript/Json/json_basics/main.js
+++ JAvascript/Json/json_basics/main.js
@@ -1,1 +1,67 @@
-//Make your own json object and see how to import it here. Then do something with it in the dom. SAve as codesnippet ie Dom and Json
+/// do something with json in the dom. SAve as codesnippet ie Dom and Json (name, age, job). No trailing commas in the json object
+
+
+let jsonText = `{ "jsonPeople" : [
+{ "firstName":"John" ,
+ "lastName":"Doe", 
+"age": 26,
+"job": "doctor"}, 
+{ "firstName":"Anna" , 
+"lastName":"Smith",
+"age": 30, 
+"job":"lawyer" }, 
+{ "firstName":"Peter" ,
+ "lastName":"Jones",
+"age": 35,
+"job":"firefighter"},
+{ "firstName":"Thomas" ,
+ "lastName":"Freddy",
+"age": 21,
+"job":"firefighter"} ,
+{ "firstName":"Melissa" ,
+ "lastName":"Jones",
+"age": 22,
+"job":"Teacher"} ,
+{ "firstName":"Omari" ,
+ "lastName":"Thomas",
+"age": 40,
+"job":"Construction Worker"}]}`; 
+
+let people = JSON.parse(jsonText)
+console.log(typeof people)
+let peopleEl = document.getElementById("people")
+
+// author_quotes_list.map(function(quote){
+
+     
+//     output += `
+//             <div class="card">
+//             <div class="card-body">
+//               ${quote}
+//             </div>
+//             </div>          
+//               `;
+//     quoteEl.innerHTML = output;}
+
+// <div class="card" style="width: 18rem;">
+//   <div class="card-body">
+//     <h5 class="card-title">fristname last name</h5>
+//     <h6 class="card-subtitle mb-2 text-muted">age</h6>
+//     <p class="card-text">this is {firtname} {lastname} and s/he works at {job}</p>
+//   </div>
+// </div>
+
+//Remember to get the person out of th eobject, it is an object with an array in it. Get the array out the object
+
+let output = "";
+people.jsonPeople.map(person => {
+    console.log(person)
+    output += `
+        <div class = "card" style = "width: 18rem;">
+            <div class = "card-body">
+                <h5 class = "card-title">${person.firstName}</h5>
+            </div>
+        </div>`;
+    peopleEl.innerHTML = output
+} )
+
