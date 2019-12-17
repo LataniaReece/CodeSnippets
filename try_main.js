7ebab677089449caf9e0eecc8ea74cea511f79eb
RTania118 <48932236+RTania118@users.noreply.github.com>
2019-12-17 14:59:16 -0500

    Create try_main.js

---
+++ try_main.js
@@ -0,0 +1,26 @@
+document.getElementById("btn").addEventListener("click", myFunction)
+
+function myFunction() {
+  console.log("click")
+  var message, x;
+  message = document.getElementById("p01");
+  message.innerHTML = "";
+  x = document.getElementById("demo").value;
+  try { 
+    if(x == "")  throw "empty";
+    if(isNaN(x)) throw "not a number";
+    x = Number(x);
+    
+    if (x < 5) {
+      throw "too low"
+    } else if (x > 10) {
+      throw "too high"
+    } else {
+      throw "good"
+    };
+  }
+  catch(err) {
+    message.innerHTML = "Input is " + err;
+  }
+}
+
