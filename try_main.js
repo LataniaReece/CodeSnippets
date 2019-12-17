947a8e913a55c8b8461a9c30e6bf4255c80758fe
RTania118 <48932236+RTania118@users.noreply.github.com>
2019-12-17 15:04:04 -0500

    Update try_main.js

--- try_main.js
+++ try_main.js
@@ -24,3 +24,36 @@ function myFunction() {
   }
 }
 
+
+// Clears the input box, finally is ran regardless of try catch results 
+// document.getElementById("btn").addEventListener("click", myFunction)
+
+// function myFunction() {
+//   console.log("click")
+//   var message, x;
+//   message = document.getElementById("p01");
+//   message.innerHTML = "";
+//   x = document.getElementById("demo").value;
+//   try { 
+//     if(x == "")  throw "empty";
+//     if(isNaN(x)) throw "not a number";
+//     x = Number(x);
+    
+//     if (x < 5) {
+//       throw "too low"
+//     } else if (x > 10) {
+//       throw "too high"
+//     } else {
+//       throw "good"
+//     };
+//   }
+//   catch(err) {
+//     message.innerHTML = "Input is " + err;
+//   }
+//   finally {
+//     document.getElementById("demo").value = "removed";
+//   }
+ 
+// }
+
+
