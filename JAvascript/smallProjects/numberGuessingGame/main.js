1b0dda8e600ea3b23dbeff8764ff5bf1c1dea96d
YOUR_USERNAME <latania.reece@outlook.com>
2019-12-20 07:53:09 -0500

    morning update

--- JAvascript/smallProjects/numberGuessingGame/main.js
+++ JAvascript/smallProjects/numberGuessingGame/main.js
@@ -9,50 +9,85 @@ submitEl.addEventListener("click", getGuess);
 
 //Setting the random number between 1 and 100
 let random_number = Math.floor(Math.random() *100) + 1;
-console.log(random_number);
+console.log(typeof random_number);
+console.log(random_number)
 
 
 counter = 10
+let guess_array = [];
+console.log(guess_array)
 //Function when the person submits something
 function getGuess(e){
     e.preventDefault()   
  
     //capture the guess value
-    let guess_value = guessEl.value;
+    let guess_value = Number(guessEl.value);
+    
 
     //Check if guess value equals the random number
-    let output = "";    
-    if(random_number == guess_value){
+    let output = ""; 
+    let isCountChange = "";
+    if(guess_value == "" || isNaN(guess_value)){
+        output += `<div class = "mx-auto" style="width: 400px;">
+        <div class = "alert alert-warning">PLEASE ENTER A NUMBER</div>
+        </div>
+        `;
+        isCountChange = false;
+    }else if(guess_array.includes(guess_value)){
+        console.log(guess_array)
+        output += `<div class = "mx-auto" style="width: 400px;">
+        <div class = "alert alert-warning">YOU GUESSED ${guess_value} ALREADY, PLEASE TRY ANOTHER NUMBER</div>
+        </div>
+        `;
+        isCountChange = false;
+    }else if(random_number == guess_value){
         output +=  `
-                    <div class = "alert alert-primary">
-                        <h3>Results: YOU WIN! THE NUMBER WAS ${guess_value}</h3>
-                    </div>  
+                    <div class = "mx-auto" style="width: 400px;">
+                        <div class = "alert alert-success">
+                            <h3>Results: YOU WIN! THE NUMBER WAS ${guess_value}</h3>
+                        </div> 
+                    </div> 
                     `; 
-        resultsEl.innerHTML = output;
+        isCountChange = true;
     }else if (random_number < guess_value){
         output +=  `
-        <div class = "alert alert-primary">
-            <h3>Results: WRONG, ${guess_value} IS TOO HIGH!</h3>
-        </div>  
+        <div class = "mx-auto" style="width: 400px;">
+            <div class = "alert alert-info">
+                <h3>Results: WRONG, ${guess_value} IS TOO HIGH!</h3>
+            </div>  
+        </div>
         `; 
-        resultsEl.innerHTML = output;
+        isCountChange = true;
     }else {
         output +=  `
-        <div class = "alert alert-primary">
-            <h3>Results: WRONG, ${guess_value} IS TOO LOW!</h3>
-        </div>  
+        <div class = "mx-auto" style="width: 400px;">
+            <div class = "alert alert-info">
+                <h3>Results: WRONG, ${guess_value} IS TOO LOW!</h3>
+            </div>  
+        </div>
         `; 
-        resultsEl.innerHTML = output;
+        isCountChange = true;
     }
-
+    resultsEl.innerHTML = output;
     //Should also add something if ti isn't a number,and if thye weren;t in the range that you suggested. 
-    //Counter 
-    current_count = counterNumEl.innerHTML;
-    counterNumEl.innerHTML = current_count - 1
+    //Counter
+    if(!isCountChange){
+        return;
+    }else{
+        current_count = counterNumEl.innerHTML;
+        counterNumEl.innerHTML = current_count - 1
+    }
+   
     
     //If they hit the submit button when it's 1 they have no more tries left
     if(current_count == 1){
         console.log("You lost")
     }
+    guess_array.push(guess_value)
+    console.log(guess_array)
+
     
 }
+
+
+//Figure out a way to have a last guess, put that into a variable and then have the new guess compare to the last guess and if it is the same then it says, you guessed that number already. Try again. I can make an array that captures all the guesses?
