8011b69162973ac5b2eeb5014f13d98b6c87afd5
YOUR_USERNAME <latania.reece@outlook.com>
2020-01-07 07:51:55 -0500

    nightly update

--- JAvascript/promises/ownPromise/main.js
+++ JAvascript/promises/ownPromise/main.js
@@ -1,4 +1,5 @@
 let btn = document.getElementById("generateWordBtn")
+
 btn.addEventListener("click", getWord)
 
 let randomWord = '';
@@ -11,6 +12,7 @@ function getWord() {
   let randomWords = wordsNumbers.filter(item => isNaN(item))
   randomWord = randomWords[Math.floor(Math.random()*randomWords.length)]
   let endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${randomWord}`;
+  console.log(endpoint)
   return fetch(endpoint)
 })
 .then(res => res.json())
@@ -21,13 +23,18 @@ function getWord() {
 .catch(err => console.log(err))
 }
 
+let functionCallCount = 0
 let displayResults = function(results) {
 
-  //Displaying the word 
+  //if this is the first time that the function is ran 
+  if(functionCallCount == 0){
+    console.log(functionCallCount)
+     //Displaying the word 
+  let mainEl = document.getElementById("mainContent")
   const wordEl = document.createElement("p");
   wordEl.id = "randomWord"
   wordEl.innerHTML = randomWord
-  btn.insertAdjacentHTML("afterend", wordEl.outerHTML)
+  btn.insertAdjacentHTML("afterEnd", wordEl.outerHTML)
 
   //Displaying the results: 
    let searchResults = document.createElement('div');
@@ -42,5 +49,41 @@ let displayResults = function(results) {
        </div>`
    }).join("");
 
-   document.body.appendChild(searchResults)
+  //  document.body.appendChild(searchResults)
+  mainEl.insertAdjacentHTML("beforeEnd", searchResults.outerHTML)
+  functionCallCount ++;
+
+  }else if (functionCallCount > 0){
+    console.log(functionCallCount)
+     //IF this is the second time that the function is ran
+
+  //Keep creating a new p element
+  const wordEl = document.createElement("p");
+  wordEl.id = "randomWord"
+  wordEl.innerHTML = randomWord
+  btn.insertAdjacentHTML("afterend", wordEl.outerHTML)
+
+  //insert after the new p element that was created, should now be the first p element in the page? or just insert after word el?
+
+  let searchResults = document.createElement('div');
+  searchResults.innerHTML += results.map(result => {
+    const url = encodeURI(`https://en.wikipedia.org/wiki/${result.title}`);
+      return `<div class="resultItem">
+        <h3 class="resultItem-title">
+          <a href="${url}" target="_blank" rel="noopener">${result.title}</a>
+        </h3>
+        <span class="resultItem-snippet">${result.snippet}</span><br>
+        <a href="${url}" class="resultItem-link" target="_blank" rel="noopener">${url}</a>
+      </div>`
+  }).join("");
+
+ //  document.body.appendChild(searchResults)
+ wordEl.insertAdjacentHTML("afterEnd", searchResults.outerHTML) //Figure out how to insert this where you want it to be
+
 }
+
+  }
+  
+
+
+  
