6b88bb83d37fda07a918f55dc36ccfeac14514d7
YOUR_USERNAME <latania.reece@outlook.com>
2020-01-08 23:30:11 -0500

    nightly update

--- JAvascript/smallProjects/comingSoon_1/main.js
+++ JAvascript/smallProjects/comingSoon_1/main.js
@@ -1,38 +1,30 @@
-var endDate = new Date("Jan 18, 2020 12:00:00").getTime();
+//Determine the final date
+let endDate = new Date("Jan 18, 2020 12:00:00").getTime();
 
-var timer = setInterval(function() {
+
+let timer = setInterval(function() {
 
     let now = new Date().getTime();
     let t = endDate - now;
     
+    //If there is time left over
     if (t >= 0) {
     
+        //t is currently in UTC format, have to transform it to days, hours, mins and secs
         let days = Math.floor(t / (1000 * 60 * 60 * 24));
         let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-        let secs = Math.floor((t % (1000 * 60)) / 1000);
-        
-      
-        // console.log(("0"+hours).slice(-2), hours)
-      
+        let secs = Math.floor((t % (1000 * 60)) / 1000);      
     
-        document.getElementById("timer-days").innerHTML = `<span class = "number">${days}</span> <span class='label'>Day(s)</span>`;
-      
+    
+        // insert the result into the document
+      document.getElementById("timer-days").innerHTML = `<span class = "number">${days}</span> <span class='label'>Day(s)</span>`;
       document.getElementById("timer-hours").innerHTML = `<span class = "number">${hours}</span> <span class='label'>Hour(s)</span>`;
-      
       document.getElementById("timer-mins").innerHTML = `<span class = "number">${mins}</span> <span class='label'>Min(s)</span>`;
-      
       document.getElementById("timer-secs").innerHTML = `<span class = "number">${secs}</span> <span class='label'>Sec(s)</span>`;
-      
-//         document.getElementById("timer-hours").innerHTML = ("0"+hours).slice(-2) +
-//         "<span class='label'>HR(S)</span>";
+
     
-//         document.getElementById("timer-mins").innerHTML = ("0"+mins).slice(-2) +
-//         "<span class='label'>MIN(S)</span>";
-    
-//         document.getElementById("timer-secs").innerHTML = ("0"+secs).slice(-2) +
-//         "<span class='label'>SEC(S)</span>";
-    
+      //If there is no time left over
     } else {
 
         document.getElementById("timer").innerHTML = "The countdown is over!";
@@ -40,3 +32,4 @@ var timer = setInterval(function() {
     }
     
 }, 1000);
+//This function will run every second
