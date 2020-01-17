752265296c434c13ea4afcaf3e4cc9fb233197a2
YOUR_USERNAME <latania.reece@outlook.com>
2020-01-16 23:30:22 -0500

    nightly update

--- JAvascript/test/main2.js
+++ JAvascript/test/main2.js
@@ -1,95 +1,314 @@
-myCards = [
-    {"suit":"clubs","value":4},
-    {"suit":"clubs","value":5},
-    {"suit":"clubs","value":6},
-{"suit":"diams","value":10},
-{"suit":"diams","value":6}]
+let playersEl = document.getElementById("poker-players")
+let players = [
 
+     {
+        playerNum: 1,
+        name: "Fox", 
+        amountLeft: 3.46,
+        cards:{
+            card1:{
+                suit: "&#9829;",
+                value: 1
+            },
+            card2:{
+                suit: "&#9829;",
+                value: 2
+            },
+            card3:{
+                suit: "&#9827;",
+                value: 3
+            },
+            card4:{
+                suit: "&diams;",
+                value: 4
+            },
+        },
+        dealer: "D"
+    },
 
-let returnGap = function(x,y){
-    //this is where we need to find and use a switch case to set x or y and set the value there then do the 
-    if(x == 'A'){ x = 14;}
-    if(x == 'K'){ x = 13;}
-    if(x == 'Q'){ x = 12;}
-    if(x == 'J'){ x = 11;}
-    if(x == 'T'){ x = 10;}
-    if(y == 'A'){ x = 14;}
-    if(y == 'K'){ x = 13;}
-    if(y == 'Q'){ x = 12;}
-    if(y == 'J'){ x = 11;}
-    if(y == 'T'){ x = 10;}
-    if(x > y){
-            return x - y
-            }else {
-            return y - x
-            }
-    };
+{
+    playerNum: 2,
+        name: "Monkey", 
+        amountLeft: 11.18,
+        cards:{
+            card1:{
+                suit: "&diams;",
+                value: 1
+            },
+            card2:{
+                suit: "&#9829;",
+                value: 2
+            },
+            card3:{
+                suit: "&#9827;",
+                value: 3
+            },
+            card4:{
+                suit: "&diams;",
+                value: 4
+            },
+        },
+        dealer: "D"
+    
+    },
+    {
+        playerNum: 3,
+        name: "Rowdy-Robert", 
+        amountLeft: 10.00,
+        cards:{
+            card1:{
+                suit: "&#9829;",
+                value: 1
+            },
+            card2:{
+                suit: "&#9829;",
+                value: 2
+            },
+            card3:{
+                suit: "&#9827;",
+                value: 3
+            },
+            card4:{
+                suit: "&diams;",
+                value: 4
+            },
+        },
+        dealer: "D"
+    
+    },
+     {
+        playerNum: 4,
+        name: "Ellie-Bellie", 
+        amountLeft: 6.00,
+        cards:{
+            card1:{
+                suit: "&#9829;",
+                value: 1
+            },
+            card2:{
+                suit: "&#9829;",
+                value: 2
+            },
+            card3:{
+                suit: "&#9827;",
+                value: 3
+            },
+            card4:{
+                suit: "&diams;",
+                value: 4
+            },
+        },
+        dealer: "D"
+    
+    },
+     {
+        playerNum: 5,
+        name: "fly_birdie", 
+        amountLeft: 15.00,
+        cards:{
+            card1:{
+                suit: "&diams;",
+                value: 1
+            },
+            card2:{
+                suit: "&#9829;",
+                value: 2
+            },
+            card3:{
+                suit: "&#9827;",
+                value: 3
+            },
+            card4:{
+                suit: "&diams;",
+                value: 4
+            },
+        },
+        dealer: "D"
+    
+    },
+    {
+        playerNum: 6,
+        name: "User", 
+        amountLeft: 6.00,
+        cards:{
+            card1:{
+                suit: "&#9829;",
+                value: 1
+            },
+            card2:{
+                suit: "&#9829;",
+                value: 2
+            },
+            card3:{
+                suit: "&#9827;",
+                value: 3
+            },
+            card4:{
+                suit: "&diams;",
+                value: 4
+            },
+        },
+        dealer: "D"
+    
+    },
+    {
+        playerNum: 7,
+        name: "Marie-Lou", 
+        amountLeft: 1.24,
+        cards:{
+            card1:{
+                suit: "&#9829;",
+                value: 1
+            },
+            card2:{
+                suit: "&#9829;",
+                value: 2
+            },
+            card3:{
+                suit: "&#9827;",
+                value: 3
+            },
+            card4:{
+                suit: "&diams;",
+                value: 4
+            },
+        },
+        dealer: "D"
+    
+    },
+     {
+         playerNum: 8,
+        name: "Albert459", 
+        amountLeft: 5.00,
+        cards:{
+            card1:{
+                suit: "&#9829;",
+                value: 1
+            },
+            card2:{
+                suit: "&#9829;",
+                value: 2
+            },
+            card3:{
+                suit: "&#9827;",
+                value: 3
+            },
+            card4:{
+                suit: "&diams;",
+                value: 4
+            },
+        },
+        dealer: "D"
+    
+    },
+    {
+        playerNum: 9,
+        name: "Vincent202", 
+        amountLeft: 2.88,
+        cards:{
+            card1:{
+                suit: "&#9829;",
+                value: 1
+            },
+            card2:{
+                suit: "&#9829;",
+                value: 2
+            },
+            card3:{
+                suit: "&#9827;",
+                value: 3
+            },
+            card4:{
+                suit: "&diams;",
+                value: 4
+            },
+        },
+        dealer: "D"
+    
+    },
+]
+let playerContainer = "";
+players.map(player => {
 
-    //This is how you determine the gap of the cards :
-// console.log(returnGap(myCards[0].value, myCards[3].value))
-
-//Determine if you have an adequate gap between your own cards, 
-// then check each card's connection with the board
-//Then check the board's connection with each other?
-
-
-// let checkforStraight = function(cards){
-//     boardArray=[];
-//     for(var i=0 ; i < 5; i++) { 
-//            value=cards[i].value;
-//            boardArray.push(value);
-//            }
-//     sortBoard=boardArray.sort((a, b) => b - a);
-//     console.log(sortBoard)
-// }
-
-// test  = checkforStraight(myCards)
-// console.log(test)
-
-//I think theres a way to reduce this so you can get the return gap for each
-
-function pairwise(list) {
-
-    var pairs = new Array((list.length * (list.length - 1)) / 2),
-        pos = 0;
-
-    for (var i = 0; i < list.length; i++) {
-        for (var j = i + 1; j < list.length; j++) {
-            pairs[pos++] = [list[i], list[j]];
-        }
-    }
-    return pairs;
-}
-
-//Get pairwise comparison
-cardValues = myCards.map(card => card.value)
-cardPairs = pairwise(cardValues)
-
-//find gap between each pairwise comparison
-cardPairs.map(cardPair => {
-    console.log(returnGap(cardPair[0], cardPair[1]))
+    playerContainer += `
+    <!--player template repeat container-->
+              <li id="player-${player.playerNum}-container" label="repeat-region">
+                <!--${player.name} Dollar Amount-->
+                <ul id="player-${player.playerNum}-dollar-amount">
+                  <h3>${player.name}</h3>
+                  <p>$${player.amountLeft}</p>
+                </ul>
+                <!--${player.name} Chip Container-->
+                <ul id="player-${player.playerNum}-chip-container" class="player-chip-container">
+                  <li class="villian-dealer-button">
+                    <div class="chips">
+                      <p>${player.dealer}</p>
+                    </div>
+                  </li>
+                  <li class="villian-big-blind">
+                    <div class="chips"></div>
+                    <div class="chips"></div>
+                    <div class="chips"></div>
+                    <div class="chips"></div>li>
+                  <li class="villian-small-blind">
+                    <div class="chips"></div>
+                    <div class="chips"></div>
+                  </li>
+                </ul>
+                <!--${player.name} Cards Container-->
+                <ul id="player-${player.playerNum}-cards-container" class="player-cards-container">
+                <li id="player-${player.playerNum}-card-1" class="card">
+                  <p id="board-card-2-value">${player.cards.card1.value}</p>
+                  <span id="board-card-2-suit_1">${player.cards.card1.suit}</span> 
+                  <span id="board-card-2-suit_2">${player.cards.card1.suit}</span>
+                </li>
+                <li id="player-${player.playerNum}-card-2" class="card">
+                  <p id="board-card-2-value">${player.cards.card2.value}</p>
+                  <span id="board-card-2-suit_1">${player.cards.card2.suit}</span> 
+                  <span id="board-card-2-suit_2">${player.cards.card2.suit}</span>
+                </li>
+                </ul>
+            </li>`    
 })
 
 
-//OKay this works but you need to figure out a way to make sure that you are aware of which cards are speicifically causing which gap. not sure how to do this yet
 
+console.log(playerContainer)
+playersEl.innerHTML = playerContainer
 
-
-
-
-// let straightDraw = function(cards){
-    
-//     //Gap between players cards :
-//     let playerHoldingsGap = returnGap(cards[0].value, cards[1].value);
-//     if(playerHoldingsGap < 4 ){
-//         console.log(playerHoldingsGap)
-//         console.log("Good gap")
-//     }else {
-//         console.log(playerHoldingsGap)
-//         console.log("bad gap")
-
-//     }
-
-// };
-
-// straightDraw(myCards)
+// let playerContainer =`
+// <!--player template repeat container->
+//           <li id="player-7-marie-lou-container" label="repeat-region">
+//             <!--Marie Lou Dollar Amount-->
+//             <ul id="marie-lou-dollar-amount">
+//               <h3>Marie-Lou</h3>
+//               <p>$1.24</p>
+//             </ul>
+//             <!--Marie Lou Chip Container-->
+//             <ul id="player-7-marie-lou-chip-container" class="player-chip-container">
+//               <li class="villian-dealer-button">
+//                 <div class="chips">
+//                   <p>D</p>
+//                 </div>
+//               </li>
+//               <li class="villian-big-blind">
+//                 <div class="chips"></div>
+//                 <div class="chips"></div>
+//                 <div class="chips"></div>
+//                 <div class="chips"></div>li>
+//               <li class="villian-small-blind">
+//                 <div class="chips"></div>
+//                 <div class="chips"></div>
+//               </li>
+//             </ul>
+//             <!--Marie Lou Cards Container-->
+//             <ul id="player-7-marie-lou-cards-container" class="player-cards-container">
+//               <li id="player-7-marie-lou-card-1" class="card">
+//               </li>
+//               <li id="player-7-marie-lou-card-2" class="card">
+//                 <li id="player-7-marie-lou-card-1" class="card">
+//                 </li>
+//                 <li id="player-7-marie-lou-card-2" class="card">
+//                 </li>
+//             </ul>
+//             </li>`
